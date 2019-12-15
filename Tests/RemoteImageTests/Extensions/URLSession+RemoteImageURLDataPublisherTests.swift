3b23c346a698ce54435d83ec4e23e8c4098d8539
Christian Elies <chris.elies13@gmail.com>
2019-12-15 12:14:41 +0100

    test(): implemented url session + remote image url data publisher test

---
+++ Tests/RemoteImageTests/Extensions/URLSession+RemoteImageURLDataPublisherTests.swift
@@ -0,0 +1,24 @@
+//
+//  URLSession+RemoteImageURLDataPublisherTests.swift
+//  RemoteImageTests
+//
+//  Created by Christian Elies on 15.12.19.
+//
+
+import Foundation
+@testable import RemoteImage
+import XCTest
+
+final class URLSession_RemoteImageURLDataPublisherTests: XCTestCase {
+    func testDataPublisher() {
+        guard let url = URL(string: "https://google.de") else {
+            XCTFail("Could not create mock URL")
+            return
+        }
+        let urlSession: URLSession = .shared
+        let urlRequest = URLRequest(url: url)
+        let dataTaskPublisher = urlSession.dataTaskPublisher(for: urlRequest).eraseToAnyPublisher()
+        let dataPublisher = urlSession.dataPublisher(for: urlRequest)
+        XCTAssertEqual(dataPublisher.description, dataTaskPublisher.description)
+    }
+}
